ace02734b713b5850cf88ce2222554244865aa41
ThomasReyel <thomasreyel2231@gmail.com>
2026-03-21 10:36:23 -0300

    add: models de task

--- todolist/task/models.py
+++ todolist/task/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class State(models.TextChoices):
+    NOVA = 'nova', 'Nova'
+    EM_ANDAMENTO = 'em_andamento', 'Em Andamento'
+    CONCLUIDA = 'concluida', 'Concluída'
+    CANCELADA = 'cancelada', 'Cancelada'
+
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=250, blank=True)
+    deadline = models.DateField
+    completion_date = models.DateField(null=True,blank=True)
+    state = models.CharField(choices=State)
+
+
