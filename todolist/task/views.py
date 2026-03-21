62bead2b40c3681078b469b3274a6c428afe43c7
ThomasReyel <thomasreyel2231@gmail.com>
2026-03-21 16:17:44 -0300

    feat: add busca por filtro

--- todolist/task/views.py
+++ todolist/task/views.py
@@ -2,8 +2,22 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from .models import Task
 from .serializers import TaskSerializer
+from django.db import models
 
 class TaskViewSet(ModelViewSet):
-    queryset =  Task.objects.all()
     serializer_class = TaskSerializer
+    queryset = Task.objects.all()
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search_term = self.request.query_params.get('search', None)
+
+        if search_term:
+            queryset = queryset.filter(
+                models.Q(title__icontains=search_term) |
+                models.Q(description__icontains=search_term)
+            )
+        return queryset
+
+
+    
